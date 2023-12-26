c3185ad42bccc6f426fa1373bee43d2417145346
Pi <piisthebestthingintheuniverse@gmail.com>
2023-12-25 18:28:19 -0800

    fixed aimoverlay and made it a separate plugin

---
+++ files/aimoverlay.js
@@ -0,0 +1,62 @@
+const name = "Aim overlay";
+const id = "plugins.pi.aimoverlay";
+const author = "Pi";
+const version = "1.0.0";
+const versionNumber = 1000;
+const description = "Hold down the Ctrl key to see a red line that helps you aim";
+const script = () => {
+	const listForAnimalsWithAimOverlay = [61, 93, 94, 113];
+	function createAimOverlay() {
+		document.getElementById("aim-overlay").remove();
+		var aim_overlay = document.createElement("hr");
+		document.querySelector("div.game").insertBefore(aim_overlay, document.querySelector("div.game").children[0]);
+		aim_overlay.outerHTML =
+			'<hr id="aim-overlay" style="border: 1px #f32d dotted;border-image: linear-gradient(to right, #f32d, #f320) 1;transform-origin: left;position: absolute;top: 50%;left: 50%;width: 40vw;display:none;pointer-events:none;">';
+	}
+	blockyfish.addEventListener("first-game-load", () => {
+		createAimOverlay();
+		document.addEventListener("mousemove", () => {
+			try {
+				if (game.currentScene != null) {
+					if (game.currentScene.myAnimal != null) {
+						document.getElementById("aim-overlay").style.transform =
+							"rotate(" + ((game.currentScene.myAnimal.inner.rotation * 180) / Math.PI + (game.currentScene.myAnimal._visibleFishLevel == 101 ? 90 : -90)) + "deg)";
+					}
+				}
+			} catch {}
+		});
+		window.addEventListener(
+			"keydown",
+			function (e) {
+				try {
+					if (e.ctrlKey && listForAnimalsWithAimOverlay.includes(game.currentScene.myAnimal._visibleFishLevel)) {
+						document.getElementById("aim-overlay").style.display = "block";
+					}
+				} catch {}
+			},
+			false
+		);
+		window.addEventListener(
+			"keyup",
+			function (e) {
+				try {
+					if (!e.ctrlKey) {
+						document.getElementById("aim-overlay").style.display = "none";
+					}
+				} catch {}
+			},
+			false
+		);
+		window.addEventListener("focus", () => {
+			try {
+				document.getElementById("aim-overlay").style.display = "none";
+			} catch {}
+		});
+	});
+
+	blockyfish.addEventListener("game-load", () => {
+		createAimOverlay();
+	});
+};
+
+module.exports = { name, id, author, version, versionNumber, description, script };
--- files/boosthacks.js
+++ files/boosthacks.js
@@ -1,76 +1,55 @@
 const name = "Boost hacks";
 const id = "plugins.pi.boosthacks";
 const author = "Pi";
-const version = "1.0.0";
-const versionNumber = 1000;
+const version = "1.0.1";
+const versionNumber = 1010;
 const description = "Ctrl+click to do a charged boost. Alt+click to do a weak charged boost. Ctrl+shift+click to do a special boost combo (only for some animals)";
 const script = () => {
-	const listForAnimalsWithAimOverlay = [61, 93, 94, 113];
-	const listForGamemodesWithAimOverlay = [1, 2, 6];
-	blockyfish.addEventListener("first-game-load", () => {
-		var ctrl_overlay = document.createElement("div");
-		document.querySelector("div.game").insertBefore(ctrl_overlay, document.querySelector("div.game").children[0]);
-		ctrl_overlay.outerHTML = '<div id="ctrl-overlay" style="width: 100%;height: 100%;position: absolute;display: block;z-index:10000;pointer-events:none;"></div>';
-		var aim_overlay = document.createElement("hr");
-		document.querySelector("div.game").insertBefore(aim_overlay, document.querySelector("div.game").children[1]);
-		aim_overlay.outerHTML =
-			'<hr id="aim-overlay" style="border: 1px #fff dotted;border-image: linear-gradient(to right, #f32d, #f320) 1;transform-origin: left;position: absolute;top: 50%;left: 50%;width: 40vw;display:none;pointer-events:none;">';
-
-		setInterval(function () {
+	function showCtrlOverlay(e) {
+		if (e.ctrlKey || e.altKey) {
 			try {
 				if (game.currentScene != null) {
 					if (game.currentScene.myAnimal != null) {
-						if (game.currentScene.myAnimal._visibleFishLevel == 101) {
-							document.getElementById("aim-overlay").style.transform = "rotate(" + ((game.currentScene.myAnimal.inner.rotation * 180) / Math.PI + 90) + "deg)";
+						if (game.currentScene.myAnimal._visibleFishLevel != 101) {
+							document.getElementById("ctrl-overlay").style.pointerEvents = "all";
+						} else if (!e.shiftKey) {
+							if (game.currentScene.myAnimal._visibleFishLevel == 101) document.getElementById("ctrl-overlay").style.pointerEvents = "all";
 						} else {
-							document.getElementById("aim-overlay").style.transform = "rotate(" + ((game.currentScene.myAnimal.inner.rotation * 180) / Math.PI - 90) + "deg)";
+							document.getElementById("ctrl-overlay").style.pointerEvents = "none";
 						}
 					}
 				}
 			} catch {}
-		}, 10);
-		function showCtrlOverlay(e) {
-			if (e.ctrlKey || e.altKey) {
-				try {
-					if (game.currentScene != null) {
-						if (game.currentScene.myAnimal != null) {
-							if (game.currentScene.myAnimal._visibleFishLevel != 101) {
-								document.getElementById("ctrl-overlay").style.pointerEvents = "all";
-							} else if (!e.shiftKey) {
-								if (game.currentScene.myAnimal._visibleFishLevel == 101) document.getElementById("ctrl-overlay").style.pointerEvents = "all";
-							} else {
-								document.getElementById("ctrl-overlay").style.pointerEvents = "none";
-							}
-						}
-					}
-				} catch {}
-			}
 		}
-		async function superShot() {
-			try {
-				game.inputManager.handleLongPress(1);
-				setTimeout(() => {
-					game.inputManager.handleLongPress(5000);
-				}, 50);
-				setTimeout(() => {
-					game.inputManager.handleLongPress(5000);
-				}, 100);
-				setTimeout(() => {
-					game.inputManager.handleLongPress(5000);
-				}, 150);
-				setTimeout(() => {
-					game.inputManager.handleLongPress(5000);
-				}, 200);
-			} catch {}
-		}
+	}
+
+	function superShot() {
+		try {
+			game.inputManager.handleLongPress(1);
+			setTimeout(() => {
+				game.inputManager.handleLongPress(5000);
+			}, 50);
+			setTimeout(() => {
+				game.inputManager.handleLongPress(5000);
+			}, 100);
+			setTimeout(() => {
+				game.inputManager.handleLongPress(5000);
+			}, 150);
+		} catch {}
+	}
+	function createCtrlOverlay() {
+		document.getElementById("ctrl-overlay").remove();
+		var ctrl_overlay = document.createElement("div");
+		document.querySelector("div.game").insertBefore(ctrl_overlay, document.querySelector("div.game").children[0]);
+		ctrl_overlay.outerHTML = '<div id="ctrl-overlay" style="width: 100%;height: 100%;position: absolute;display: block;z-index:10000;pointer-events:none;"></div>';
+	}
+	blockyfish.addEventListener("first-game-load", () => {
+		createCtrlOverlay();
 		window.addEventListener(
 			"keydown",
 			function (e) {
 				try {
 					showCtrlOverlay(e);
-					if (e.ctrlKey && listForAnimalsWithAimOverlay.includes(game.currentScene.myAnimal._visibleFishLevel) && listForGamemodesWithAimOverlay.includes(game.gameMode) && aim_helper_on) {
-						document.getElementById("aim-overlay").style.display = "block";
-					}
 				} catch {}
 			},
 			false
@@ -81,7 +60,6 @@ const script = () => {
 				try {
 					if (e.ctrlKey) {
 						if (e.shiftKey && (game.currentScene.myAnimal._visibleFishLevel == 109 || game.currentScene.myAnimal._visibleFishLevel == 107)) {
-							console.log("hi");
 							superShot();
 						} else if (e.shiftKey && game.currentScene.myAnimal._visibleFishLevel != 101) {
 							game.inputManager.handleLongPress(-5);
@@ -103,9 +81,6 @@ const script = () => {
 					if (!e.ctrlKey && !e.altKey) {
 						document.getElementById("ctrl-overlay").style.pointerEvents = "none";
 					}
-					if (!e.ctrlKey) {
-						document.getElementById("aim-overlay").style.display = "none";
-					}
 				} catch {}
 			},
 			false
@@ -113,19 +88,12 @@ const script = () => {
 		window.addEventListener("focus", () => {
 			try {
 				document.getElementById("ctrl-overlay").style.pointerEvents = "none";
-				document.getElementById("aim-overlay").style.display = "none";
 			} catch {}
 		});
 	});
 
 	blockyfish.addEventListener("game-load", () => {
-		var ctrl_overlay = document.createElement("div");
-		document.querySelector("div.game").insertBefore(ctrl_overlay, document.querySelector("div.game").children[0]);
-		ctrl_overlay.outerHTML = '<div id="ctrl-overlay" style="width: 100%;height: 100%;position: absolute;display: block;z-index:10000;pointer-events:none;"></div>';
-		var aim_overlay = document.createElement("hr");
-		document.querySelector("div.game").insertBefore(aim_overlay, document.querySelector("div.game").children[1]);
-		aim_overlay.outerHTML =
-			'<hr id="aim-overlay" style="border: 1px #fff dotted;border-image: linear-gradient(to right, #f32d, #f320) 1;transform-origin: left;position: absolute;top: 50%;left: 50%;width: 40vw;display:none;pointer-events:none;">';
+		createCtrlOverlay();
 	});
 };
 
