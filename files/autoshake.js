9c87e5f059d51c0618f1ffc7d5dc752a4d752a74
Pi <piisthebestthingintheuniverse@gmail.com>
2024-01-20 12:33:30 -0800

    optimized autoshake plugin

--- files/autoshake.js
+++ files/autoshake.js
@@ -1,8 +1,8 @@
 const name = "AutoShake";
 const id = "plugins.thej.autoshake";
 const author = "TheJ";
-const version = "1.0.2";
-const versionNumber = 1020;
+const version = "1.0.3";
+const versionNumber = 1030;
 const description = "Press V to toggle autoshake (sends packets to server to imitate mouse shaking forward backward doesnt alter rotation) (can be used to walk around invisible as GPO)";
 const script = () => {
 	/*
@@ -18,6 +18,7 @@ const script = () => {
 	**********
     
     */
+	var inter;
 	blockyfish.addEventListener("first-game-load", () => {
 		let KEYBIND = "KeyV";
 		let TOGGLE = false;
@@ -32,23 +33,26 @@ const script = () => {
 				) {
 					TOGGLE = !TOGGLE;
 					game.currentScene.showMessagePopup(`Autoshake has been ${TOGGLE ? "enabled" : "disabled"}`, 3000, false);
+					if (TOGGLE) {
+						inter = setInterval(() => {
+							try {
+								if (window["game"] == null) return;
+								if (game.currentScene == null || game.currentScene.myAnimal == null || game.inputManager == null || game.inputManager.getMouseWorldPosition == null) return;
+								let mpos = game.inputManager.getMouseWorldPosition();
+								let ppos = game.currentScene.myAnimal.position;
+								let angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
+								let incr = FORWARD ? 1000 : 750;
+								let bpos = [ppos._x + Math.cos(angleRadians) * incr, ppos._y + Math.sin(angleRadians) * incr];
+								game.socketManager.sendBytePacket(blockyfish.formMovePacket(bpos[0], bpos[1], 100));
+								FORWARD = !FORWARD;
+							} catch {}
+						}, 40);
+					} else {
+						clearInterval(inter);
+					}
 				}
 			} catch {}
 		});
-		setInterval(() => {
-			try {
-				if (window["game"] == null) return;
-				if (!TOGGLE) return;
-				if (game.currentScene == null || game.currentScene.myAnimal == null || game.inputManager == null || game.inputManager.getMouseWorldPosition == null) return;
-				let mpos = game.inputManager.getMouseWorldPosition();
-				let ppos = game.currentScene.myAnimal.position;
-				let angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
-				let incr = FORWARD ? 1000 : 750;
-				let bpos = [ppos._x + Math.cos(angleRadians) * incr, ppos._y + Math.sin(angleRadians) * incr];
-				game.socketManager.sendBytePacket(blockyfish.formMovePacket(bpos[0], bpos[1], 100));
-				FORWARD = !FORWARD;
-			} catch {}
-		}, 40);
 	});
 };
 
