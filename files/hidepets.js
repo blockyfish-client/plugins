4b171913d8772596bb6396554a812611ab4164d9
Pi <piisthebestthingintheuniverse@gmail.com>
2024-01-28 15:51:00 -0800

    fixed hidepets plugin hiding shells

--- files/hidepets.js
+++ files/hidepets.js
@@ -1,15 +1,18 @@
 const name = "Hide pets";
 const id = "plugins.pi.hidepets";
 const author = "Pi";
-const version = "1.0.0";
-const versionNumber = 1000;
+const version = "1.1.0";
+const versionNumber = 1100;
 const description = "Hides all pets in the game";
 const script = () => {
 	blockyfish.addEventListener("first-game-load", () => {
-		game.currentScene.justAboveAnimalsContainer.visible = false;
-	});
-	blockyfish.addEventListener("game-load", () => {
-		game.currentScene.justAboveAnimalsContainer.visible = false;
+		setInterval(() => {
+			try {
+				game.currentScene.justAboveAnimalsContainer.children.forEach((p) => {
+					if (!p.foodData) p.visible = false;
+				});
+			} catch {}
+		}, 300);
 	});
 };
 
